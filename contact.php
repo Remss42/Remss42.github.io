e31d0d6a34c47a75cd01f0e2949b31caa07940a7
Remss42 <122833883+Remss42@users.noreply.github.com>
2023-06-06 15:20:15 +0200

    Add files via upload

--- contact.php
+++ contact.php
@@ -1,20 +1,3 @@
-<?php
-//get data from form  
-$name = $_POST['name'];
-$email= $_POST['email'];
-$message= $_POST['message'];
-$to = "remi.oms42@gmail.com";
-$subject = "Mail From website Video game";
-$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
-$headers = "From: noreply@yoursite.com" . "\r\n" .
-"CC: somebodyelse@example.com";
-if($email!=NULL){
-    mail($to,$subject,$txt,$headers);
-}
-//redirect
-header("Location:index.html");
-?>
-
 
 
 <?php
